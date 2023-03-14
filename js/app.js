8b9320527422eb63719aa3115160cb7721bc9b6b
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-03-14 03:09:03 +0100

    mark up hmtl and css

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//SELECTING DOM ELEMENTS
+
+const typeEl = document.querySelector(".type");
+
+// Create an array of words we would like to display
+const words = ["Developer", "UI-designer", "Devops-Engineer", "SQL-Analyst"];
